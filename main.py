b60dc630bd236b0afc1203f9de135b532b581abe
Sahar <shr.farahzad@gmail.com>
2025-01-04 20:38:32 +0330

    Add a simple script to demonstrate Python data types

---
+++ main.py
@@ -0,0 +1,9 @@
+book1 = "Learning Python"
+book1_price = 10
+book1_quantity = 5
+book1_total_price = book1_price * book1_quantity
+
+print(type(book1))  # Output: <class 'str'>
+print(type(book1_price))  # Output: <class 'int'>
+print(type(book1_quantity))  # Output: <class 'int'>
+print(type(book1_total_price))  # Output: <class 'int'>
