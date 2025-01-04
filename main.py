6ef4678dc06c66101a67ba0c735311932d9d3d28
Sahar <shr.farahzad@gmail.com>
2025-01-04 22:51:57 +0330

    Refactor to use instance attributes

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ class Book:
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x * y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 book1 = Book("Learning Python", 10, 5)
@@ -14,7 +14,9 @@ book2 = Book("Two Scoops of Django", 30, 10)
 print(book1.title)  # Output: Learning Python
 print(book1.price)  # Output: 10
 print(book1.quantity)  # Output: 5
+print(book1.calculate_total_price())  # Output: 50
 
 print(book2.title)  # Output: Two Scoops of Django
 print(book2.price)  # Output: 30
 print(book2.quantity)  # Output: 10
+print(book2.calculate_total_price())  # Output: 300
